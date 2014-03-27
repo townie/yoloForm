87f5e9cfc8571c4d061ab6cf8a7b19195ca09763
Keith Webber <keith.e.webber@gmail.com>
2014-03-27 17:43:20 -0400

    added validation

--- app/models/cform.rb
+++ app/models/cform.rb
@@ -1,2 +1,8 @@
 class Cform < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :description, presence: true
+  validates :title, presence: true
+
 end
