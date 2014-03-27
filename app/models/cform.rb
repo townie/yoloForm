f22e930793f42621fcdebc2b4891693ae9526dc5
Daniel Greenberg <dgreenbe777@gmail.com>
2014-03-27 18:20:37 -0400

    unit tests complete with boundaries

--- app/models/cform.rb
+++ app/models/cform.rb
@@ -1,2 +1,7 @@
 class Cform < ActiveRecord::Base
+  validates :first_name, presence: true, format: { with: /[a-z]+/i }
+  validates :last_name, presence: true, format: { with: /[a-z]/i }
+  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create }, uniqueness: true
+  validates :subject, presence: true
+  validates :description, presence: true
 end
