3a48be3ab490f92ef5f0505413245b35d56dc5cd
Daniel Greenberg <dgreenbe777@gmail.com>
2014-03-27 18:27:03 -0400

    fixed regex

--- app/models/cform.rb
+++ app/models/cform.rb
@@ -1,6 +1,6 @@
 class Cform < ActiveRecord::Base
-  validates :first_name, presence: true, format: { with: /[a-z]+/i }
-  validates :last_name, presence: true, format: { with: /[a-z]/i }
+  validates :first_name, presence: true, format: { with: /[^\d]/ }
+  validates :last_name, presence: true, format: { with: /[^\d]/i }
   validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create }, uniqueness: true
   validates :subject, presence: true
   validates :description, presence: true
